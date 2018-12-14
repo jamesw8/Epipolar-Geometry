aa6e396616e6dbd5be885bd1cf96be8bf6538e98
jwong008@citymail.cuny.edu <jwong008@citymail.cuny.edu>
2018-12-14 18:23:13 -0500

    Added main thread with threads for each algorithm

---
+++ main_thread.py
@@ -0,0 +1,19 @@
+import argparse
+import threading
+from subprocess import call
+
+
+# Script calls
+def call_draw(algorithm):
+	call(["python", "draw.py", "-a", algorithm])
+
+if __name__ == '__main__':
+	thread1 = threading.Thread(target=call_draw, args=('7point',))
+	thread2 = threading.Thread(target=call_draw, args=('8point',))
+	thread3 = threading.Thread(target=call_draw, args=('RANSAC',))
+	thread4 = threading.Thread(target=call_draw, args=('LMEDS',))
+	thread1.start()
+	thread2.start()
+	thread3.start()
+	thread4.start()
+
