ca27f21dd75abc7d7c6a82cca3da8d1081852570
jwong008@citymail.cuny.edu <jwong008@citymail.cuny.edu>
2018-12-14 18:23:29 -0500

    Added arg parser to specify which algorithm

--- draw.py
+++ draw.py
@@ -8,10 +8,47 @@ from PIL import ImageTk, Image
 import numpy as np
 import os
 import random
+import argparse
+
+# Import fundamental matrix algorithms
+from fundamental import calculate_8point_fundamental_matrix, \
+						calculate_7point_fundamental_matrix, \
+						calculate_RANSAC_fundamental_matrix, \
+						calculate_LMEDS_fundamental_matrix,  \
+						normalize_points,					 \
+						calculateEpipoles
 
 from pprint import pprint
 
+# Parse arguments
+def get_args():
+	parser = argparse.ArgumentParser(description='Process some integers.')
+	parser.add_argument('-a', '--algorithm', choices=['7point', '8point', 'RANSAC', 'LMEDS'],
+	                    help='algorithm used to calculate fundamental matrix', required=True)
+	
+	args = parser.parse_args()
+	return args
+
+algorithm_name = get_args().algorithm
+
+# Get algorithm function from argument
+def get_algorithm_from_args(algorithm_name):
+	if algorithm_name == '7point':
+		return calculate_7point_fundamental_matrix
+	elif algorithm_name == '8point':
+		return calculate_8point_fundamental_matrix
+	elif algorithm_name == 'RANSAC':
+		return calculate_RANSAC_fundamental_matrix
+	elif algorithm_name == 'LMEDS':
+		return calculate_LMEDS_fundamental_matrix
+	else:
+		assert False, "Can't find specified algorithm " + args.algorithm
+
+algorithm = get_algorithm_from_args(algorithm_name)
 debug_mode = True
+debug_p_image = [(254.0, 140.0), (393.0, 147.0), (252.0, 173.0), (397.0, 177.0), (369.0, 94.0), (400.0, 65.0), (334.0, 61.0), (267.0, 229.0), (160.0, 344.0), (381.0, 233.0), (511.0, 198.0)]
+debug_q_image = [(973.0, 209.0), (1107.0, 198.0), (970.0, 236.0), (1113.0, 231.0), (1157.0, 162.0), (1182.0, 138.0), (1122.0, 143.0), (983.0, 287.0), (730.0, 400.0), (1094.0, 286.0), (1220.0, 231.0)]
+
 test_mode = False
 normalize = True
 resize = True
@@ -75,7 +112,7 @@ def listenHover(event):
 	updateMouseCoord(event)
 
 def calculatePicture(normalize=normalize):
-	global rimg1, img2, img2_canvas, width, epilines, epipoles, F_estimate
+	global rimg1, img2, img2_canvas, width, epilines, epipoles, F_estimate, debug_p_image, debug_q_image
 	# Reset epilines and epipoles drawn from last run
 	if len(epilines) > 0:
 		for epiline in epilines:
@@ -91,16 +128,20 @@ def calculatePicture(normalize=normalize):
 
 	# TEST POINTS HERE
 	if debug_mode:
-		p_image.extend([(254.0, 140.0), (393.0, 147.0), (252.0, 173.0), (397.0, 177.0), (369.0, 94.0), (400.0, 65.0), (334.0, 61.0), (267.0, 229.0), (160.0, 344.0), (381.0, 233.0), (511.0, 198.0)])
-		q_image.extend([(973.0, 209.0), (1107.0, 198.0), (970.0, 236.0), (1113.0, 231.0), (1157.0, 162.0), (1182.0, 138.0), (1122.0, 143.0), (983.0, 287.0), (730.0, 400.0), (1094.0, 286.0), (1220.0, 231.0)])
+		p_image.extend(debug_p_image)
+		q_image.extend(debug_q_image)
 
 		p_image = [[i[0], i[1], 1] for i in p_image]
 		q_image = [[i[0]-width, i[1], 1] for i in q_image]
 
 	print("List of points p:", p_image)
 	print("List of points q:", q_image)
+	p_image = np.float64(p_image)
+	q_image = np.float64(q_image)
 
-	F_estimate = calculateFundamentalMatrix(p_image, q_image, normalize=normalize)
+	print('Using', algorithm)
+	F_estimate, mask = algorithm(p_image, q_image)#calculateFundamentalMatrix(p_image, q_image, normalize=normalize)
+	print(F_estimate)
 	# Draw epipoles (2 methods)
 		# THIS WORKS
 		# e1 = U[:,2]
@@ -124,150 +165,7 @@ def calculatePicture(normalize=normalize):
 		createEpipolarLine(F_estimate, p_image[i], width, color)
 		createEpipolarLine(np.transpose(F_estimate), q_image[i], 0, color)
 
-def calculateFundamentalMatrix(p_image, q_image, normalize):
-	p = q = None
 
-	# Normalize the points for fundamental matrix calculation
-	if normalize:
-		print('\nP\n')
-		pprint(p)
-		print('\nQ\n')
-		pprint(q)
-		p, Tp = normalize_points(np.array(p_image))
-		print('\nNew P\n')
-		pprint(p)
-		print('\nTp\n')
-		pprint(Tp)
-		q, Tq = normalize_points(np.array(q_image))
-		print('\nNew Q\n')
-		pprint(q)
-		print('\nTq\n')
-		pprint(Tq)
-	else:
-		p = p_image
-		q = q_image
-		Tp = np.identity(3)
-		Tq = np.identity(3)
-
-	A = np.empty((len(p_image), 9))
-
-	assert len(p) == len(q) >= 8, "Need at least 8 point correspondences"
-	print(len(p), len(q))
-
-	for i in range(len(p)):
-		xl = p[i][0]
-		yl = p[i][1]
-		xr = q[i][0]
-		yr = q[i][1]
-
-	
-		A[i][0] = xl * xr 	# p[i][0] * q[i][0]
-		A[i][1] = yl * xr 	# p[i][0] * q[i][1]
-		A[i][2] = xr 		# p[i][0]
-		A[i][3] = xl * yr 	# p[i][1] * q[i][0]
-		A[i][4] = yl * yr 	# p[i][1] * q[i][1]
-		A[i][5] = yr 		# p[i][1]
-		A[i][6] = xl 		# q[i][0]
-		A[i][7] = yl 		# p[i][1]
-		A[i][8] = 1
-
-	print('A rank', np.linalg.matrix_rank(A))
-	assert np.linalg.matrix_rank(A) >= 8, 'A needs at least rank 8'
-	print('A', A)
-	U, s, Vt = np.linalg.svd(A)
-	print(U.shape, Vt.shape, s.shape)
-	print('s', len(s), s)
-
-	x = Vt[-1].reshape((3, 3))
-
-	print('F_hat rank', np.linalg.matrix_rank(x))
-
-	# Test fundamental matrix equation
-	# pr^T * F * pl = 0
-	for i in range(len(p)):
-		print(i, p[i], q[i], np.dot(np.transpose(q[i]), np.dot(x, p[i])))
-
-	print('x', x.shape)
-	pprint(x)
-
-	skip_zero = False
-	# Set smallest singular value to 0 to enforce rank of 2
-	if not skip_zero:
-		print('Setting smallest singular value to 0')
-		# SVD of F_hat
-		U, s, Vt = np.linalg.svd(x)
-
-		D = np.diag([s[0], s[1], 0])
-
-		F_estimate = np.dot(U, np.dot(D, Vt))
-
-		print('F rank', np.linalg.matrix_rank(F_estimate))
-
-		if normalize:
-			# Denormalize F using transformation matrices from both sets of points
-			F_estimate = np.dot(np.transpose(Tq), np.dot(F_estimate, Tp))
-
-		# Test fundamental matrix equation
-		# pr^T * F * pl = 0
-		assert len(p_image) == len(q_image)
-		for i in range(len(p_image)):
-			print(i, p_image[i], q_image[i], np.dot(np.transpose(q_image[i]), np.dot(F_estimate, p_image[i])))
-
-		print('F_estimate', F_estimate.shape)
-		pprint(F_estimate)
-		print('Dividing by bottom rightmost')
-		F_estimate = F_estimate / F_estimate[2,2]
-		print('New F_estimate')
-		pprint(F_estimate)
-
-		# SVD of F_estimate
-		U, s, Vt = np.linalg.svd(F_estimate)
-
-		D = np.diag(s)
-		V = np.transpose(Vt)
-
-		print('U', U.shape)
-		pprint(U)
-		print('D', D.shape)
-		pprint(D)
-		print('V', V.shape)
-		pprint(V)
-
-	return F_estimate
-
-def normalize_points(points):
-	x_total = y_total = 0
-	x_count = y_count = 0
-
-	for i in (points):
-		x_total += i[0]
-		y_total += i[1]
-		x_count += 1
-		y_count += 1
-	
-	x_mean = x_total / x_count
-	y_mean = y_total / y_count
-
-	S = scaling_factor = (2**0.5) / np.std(np.concatenate([points[:,0], points[:,1]]))
-	print('Scaling factor', S)
-	# Translation matrix to normalize points
-	T = np.array([
-		[S, 0, -S*x_mean],
-		[0, S, -S*y_mean],
-		[0, 0, 1]
-	])
-	new_points = []
-	for i in range(len(points)):					
-		new_points.append(np.matmul(T, points[i]))
-	# print('Normalized', new_points)
-	return new_points, T
-# Get epipoles
-def calculateEpipoles(F):
-	U, s, Vt = np.linalg.svd(F)
-	e1 = Vt[-1]
-	U, s, Vt = np.linalg.svd(np.transpose(F))
-	e2 = Vt[-1]
-	return e1, e2
 # Draw epipole
 def drawEpipole(e, add):
 	global w, width, height, new, coord, epipoles
@@ -453,10 +351,13 @@ def getActualCoords(point):
 def updateMouseCoord(event):
 	global w, coord
 	w.itemconfigure(coord, fill='white', text='%d, %d'%(event.x, event.y))
-def main():
-	global w, width, height, new, original, arrows, coord, rimg1, img2, img2_canvas, calculateButton, epilines, epipoles, hidden, test_new, test_original, test_arrows, test_hidden, testButton, test_epilines, test_correct, F_estimate, resize, new_image_size
+def createWindow():
+	global w, width, height, new, original, arrows, coord, rimg1, img2, img2_canvas, calculateButton, \
+	epilines, epipoles, hidden, test_new, test_original, test_arrows, test_hidden, testButton, test_epilines, test_correct, \
+	F_estimate, resize, new_image_size, algorithm_name
 	# Initialize window and canvas
 	top = tkinter.Tk()
+	top.title(algorithm_name)
 	w = tkinter.Canvas(top, bd=-2)
 	w.grid(row=0, column=0)
 	# Event Listeners
@@ -528,4 +429,4 @@ def main():
 	top.mainloop()
 
 if __name__ == '__main__':
-	main()
+	createWindow()
